f60b594d32f72b33e965a496d088ce7bf4a97abf
Nurik985 <onurik1985@gmail.com>
2022-07-19 16:05:47 +0500

    Добавлен полÑуенччение маркет sku

--- api/index.php
+++ api/index.php
@@ -4,38 +4,73 @@ use Monolog\Level;
 use Nur\Yaozon\Yandex;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
 
 require_once('../vendor/autoload.php');
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+$loggerGuzzle = new Logger('requests');
+$loggerGuzzle->pushHandler(new StreamHandler(__DIR__ .'/../logs/all.log'));
 
-$log = new Logger('Debug');
-$log->pushHandler(new StreamHandler('../logs/debug.log', Level::Debug));
+$stack = HandlerStack::create();
 
-if(!empty($_REQUEST['auth-token']) &&  $_REQUEST['auth-token'] == getenv('YANDEX_AUTH_TOKEN')) {
+$stack->push(
+    Middleware::log(
+        $loggerGuzzle,
+        new MessageFormatter('{req_body} - {res_body}')
+    )
+);
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ .'/../');
+$env = $dotenv->load();
+
+$logRequest = new Logger('Debug');
+$logRequest->pushHandler(new StreamHandler(__DIR__ .'/../logs/requests.log', Level::Debug));
+
+$logStocks = new Logger('Debug');
+$logStocks->pushHandler(new StreamHandler(__DIR__ .'/../logs/stock.log', Level::Debug));
+
+$logCart = new Logger('Debug');
+$logCart->pushHandler(new StreamHandler(__DIR__ .'/../logs/cart.log', Level::Debug));
+
+$logOrderAccept= new Logger('Debug');
+$logOrderAccept->pushHandler(new StreamHandler(__DIR__ .'/../logs/order-accept.log', Level::Debug));
+
+$logOrderStatus= new Logger('Debug');
+$logOrderStatus->pushHandler(new StreamHandler(__DIR__ .'/../logs/order-status.log', Level::Debug));
+
+$logGetMarketSku= new Logger('Debug');
+$logGetMarketSku->pushHandler(new StreamHandler(__DIR__ .'/../logs/get-market-sku.log', Level::Debug));
+
+if(!empty($_REQUEST['auth-token']) &&  $_REQUEST['auth-token'] == $env['YANDEX_AUTH_TOKEN']) {
 
     $json = file_get_contents('php://input');
 
-    if($_REQUEST['action'] == 'stocks') {
-        $log->debug('Stock  ==>: '. $json);
+    if($env['DEBUG']){ $logRequest->debug('Request ==>: '. json_encode($_REQUEST)); }
 
-        $client = new GuzzleHttp\Client();
-        $response = $client->request('POST', getenv('URL_STOCKS'), [
+    if($_REQUEST['action'] === 'stocks') {
+
+        //if($env['DEBUG']){ $logStocks->debug('Stock  ==>: '. $json); }
+
+        $client = new GuzzleHttp\Client(['handler' => $stack]);
+        $response = $client->request('POST', $env['URL_STOCKS_AND_CART'], [
             'body' => $json,
             'headers' => ['Content-Type' => 'application/json']
         ]);
 
-        $res = json_decode($response->getBody()->getContents(), 1);
+        $res    = json_decode($response->getBody(), 1);
+        $warehouseId   = json_decode($json, 1);
 
-        $log->debug('Ответ для Stock  <==:  '. json_encode($res));
+        //if($env['DEBUG']){ $logStocks->debug('Ответ для Stock  <==:  '. json_encode($res)); }
 
         $nData = [];
 
         if($res){
             foreach($res['skus'] as $k => $val){
                 $nData['skus'][$k]['sku'] = $val['sku'];
-                $nData['skus'][$k]['warehouseId'] = $json['warehouseId'];
+                $nData['skus'][$k]['warehouseId'] = $warehouseId['warehouseId'];
                 $nData['skus'][$k]['items'][0]['type'] = "FIT";
                 $nData['skus'][$k]['items'][0]['count'] = $val['count'];
                 $nData['skus'][$k]['items'][0]['updatedAt'] = date("Y-m-d")."T".date("H:i:s")."+04:00";
@@ -50,45 +85,171 @@ if(!empty($_REQUEST['auth-token']) &&  $_REQUEST['auth-token'] == getenv('YANDEX
         }
     }
 
-    if($_REQUEST['action'] == 'cart'){
+    if($_REQUEST['action'] === 'cart'){
 
-        $log->debug('Cart ==>: '. $json);
+        if($env['DEBUG']){ $logCart->debug('Cart ==>: '. $json); }
 
-        $client = new GuzzleHttp\Client();
-        $response = $client->request('POST', getenv('URL_CART'), [
+        $client = new GuzzleHttp\Client(['handler' => $stack]);
+        $response = $client->request('POST', $env['URL_STOCKS_AND_CART'], [
             'body' => $json,
             'headers' => ['Content-Type' => 'application/json']
         ]);
 
-        $res = json_decode($response->getBody()->getContents(), 1);
+        $res = json_decode($response->getBody(), 1);
+
+        /*$allCount = true;
+
+        foreach($res['items'] as $k => $v){
+            if($v['count'] > 0){
+                $allCount = false;
+                break;
+            }
+        }
+
+        if($allCount === true){
+            $otvet['cart']['items'] = "";
+        } else {
+            $otvet['cart'] = $res;
+        }*/
 
         $otvet['cart'] = $res;
 
-        $log->debug('Ответ для Cart  <==: '. json_encode($otvet));
+        if($env['DEBUG']){ $logCart->debug('Ответ для Cart  <==: '. json_encode($otvet)); }
 
         echo json_encode($otvet);
 
     }
 
-    if($_REQUEST['action'] == 'order/accept'){
+    if($_REQUEST['action'] === 'order/accept'){
 
-        $log->debug('Order/accept ==>: '. $json);
+        if($env['DEBUG']){ $logOrderAccept->debug('Order/accept ==>: '. $json); }
 
-        $client = new GuzzleHttp\Client();
-        $response = $client->request('POST', getenv('URL_ORDER_ACCEPT'), [
+        $client = new GuzzleHttp\Client(['handler' => $stack]);
+        $response = $client->request('POST', $env['URL_ORDER_ACCEPT_STATUS'], [
             'body' => $json,
             'headers' => ['Content-Type' => 'application/json']
         ]);
 
-        $res = json_decode($response->getBody()->getContents(), 1);
+        $res = json_decode($response->getBody(), 1);
 
         $otvet = [];
         $otvet["order"]["accepted"] = true;
         $otvet["order"]["id"] = $res['id_zayav'][0];
 
-        $log->debug('Ответ для Order/accept  <==: '. json_encode($otvet));
+        if($env['DEBUG']){ $logOrderAccept->debug('Ответ для Order/accept  <==: '. json_encode($otvet)); }
 
         echo json_encode($otvet);
+    }
+
+    if($_REQUEST['action'] === 'order/status'){
+
+        $order = json_decode($json, 1);
+
+        if($env['DEBUG']){ $logOrderStatus->debug('Order/status №'.$order['order']['id'].' ==>: '. $json); }
+
+        $client = new GuzzleHttp\Client(['handler' => $stack]);
+        $response = $client->request('POST', $env['URL_ORDER_ACCEPT_STATUS'], [
+            'body' => $json,
+            'headers' => ['Content-Type' => 'application/json']
+        ]);
+
+        if($response->getBody()){
+
+            if($env['DEBUG']){ $logOrderStatus->debug('Ответ для Order/status №'.$order['order']['id'].'  <==: '. $response->getBody()); }
+
+            $url = $env['YANDEX_API_URL'].'/campaigns/'.$env['CAMPAIGN'].'/orders/'.$order['order']['id'].'/status.json';
+            $response = $client->request('PUT', $url, [
+                'body' => $response->getBody(),
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'OAuth oauth_token="'.$env['YANDEX_TOKEN'].'", oauth_client_id="'.$env['YANDEX_CLIENT_ID'].'"',
+                ]
+            ]);
+
+            if($env['DEBUG']){ $logOrderStatus->debug('Измененный Order/status №'.$order['order']['id'].' ==>: '. $response->getBody()); }
+
+            $response = $client->request('POST', $env['URL_ORDER_ACCEPT_STATUS'], [
+                'body' => $response->getBody(),
+                'headers' => ['Content-Type' => 'application/json']
+            ]);
+
+        } else {
+            header("HTTP/1.1 200 OK");
+        }
+    }
+
+    if($_REQUEST['action'] === 'order/cancelled'){
+
+        $order = json_decode($json, 1);
+
+        if($env['DEBUG']){ $logOrderStatus->debug('Отменяем заказ Order/status №'.$order['orderid'].'  <==: '. $json); }
+
+        $client = new GuzzleHttp\Client(['handler' => $stack]);
+        $url = $env['YANDEX_API_URL'].'/campaigns/'.$env['CAMPAIGN'].'/orders/'.$order['orderid'].'/status.json';
+        $body = [];
+        $body['order'] = $order['order'];
+        $response = $client->request('PUT', $url, [
+            'body' => json_encode($body),
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'OAuth oauth_token="'.$env['YANDEX_TOKEN'].'", oauth_client_id="'.$env['YANDEX_CLIENT_ID'].'"',
+            ]
+        ]);
+    }
+
+    if($_REQUEST['action'] === 'order/check'){
+
+        $order = json_decode($json, 1);
+
+        $client = new GuzzleHttp\Client(['handler' => $stack]);
+        $url = $env['YANDEX_API_URL'].'/campaigns/'.$env['CAMPAIGN'].'/orders/'.$order['orderid'].'.json';
+        $response = $client->request('GET', $url, [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'OAuth oauth_token="'.$env['YANDEX_TOKEN'].'", oauth_client_id="'.$env['YANDEX_CLIENT_ID'].'"',
+            ]
+        ]);
+
+        if($response->getBody()) {
+
+            $response = $client->request('POST', $env['URL_ORDER_ACCEPT_STATUS'], [
+                'body' => $response->getBody(),
+                'headers' => ['Content-Type' => 'application/json']
+            ]);
+
+        } else {
+            echo "Заказ с номером ".$order['orderid']." не найден";
+        }
+
+
+    }
+
+    if($_REQUEST['action'] === 'get/sku'){
+
+        if($env['DEBUG']){ $logGetMarketSku->debug('Shop SKU ==>: '. $json); }
+
+        $skus = json_decode($json, 1);
+
+        $client = new GuzzleHttp\Client();
+
+        $url = $env['YANDEX_API_URL'].'/campaigns/'.$env['CAMPAIGN'].'/offer-mapping-entries.json?shop_sku='.$skus['sku'][0];
+
+        $response = $client->request('GET', $url, [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'OAuth oauth_token="'.$env['YANDEX_TOKEN'].'", oauth_client_id="'.$env['YANDEX_CLIENT_ID'].'"',
+            ]
+        ]);
+
+        $res = json_decode($response->getBody(), 1);
+
+        if($res['status'] == 'OK'){
+            echo $res['result']['offerMappingEntries'][0]['mapping']['marketSku'];
+        } else {
+            echo null;
+        }
+
+
     }
 }
 
--- index.php
+++ index.php
@@ -1,1 +1,50 @@
 <?php
+
+use Monolog\Level;
+use Nur\Yaozon\Yandex;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
+
+require_once('vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$env = $dotenv->load();
+
+/*
+
+$client = new GuzzleHttp\Client();
+
+$url = 'https://api.partner.market.yandex.ru/v2/campaigns/10003/orders/7206821/delivery/parcel/4177/boxes.json';
+$url = $env['YANDEX_API_URL'].'/campaigns/'.$env['CAMPAIGN'].'/orders/121352086/delivery/parcel/214546847/boxes.json';
+
+$body = '{"boxes":[{"fulfilmentId": "121352086-1"}]}';
+
+$response = $client->request('PUT', $url, [
+    'body' => $body,
+    'headers' => [
+        'Content-Type' => 'application/json',
+        'Authorization' => 'OAuth oauth_token="AQAAAAAsVzKzAAflIiUAn89MeERNiDYYpmClAPs", oauth_client_id="6088a34ebee24bc1b9001f29ff8bf8e6"',
+    ]
+]);*/
+
+$client = new GuzzleHttp\Client();
+
+$url = $env['YANDEX_API_URL'].'/campaigns/'.$env['CAMPAIGN'].'/offer-mapping-entries.json?shop_sku=530023';
+
+$response = $client->request('GET', $url, [
+    'headers' => [
+        'Content-Type' => 'application/json',
+        'Authorization' => 'OAuth oauth_token="AQAAAAAsVzKzAAflIiUAn89MeERNiDYYpmClAPs", oauth_client_id="6088a34ebee24bc1b9001f29ff8bf8e6"',
+    ]
+]);
+
+$res = json_decode($response->getBody(), 1);
+
+
+echo "<pre>";
+print_r($res);
+echo "</pre>";
